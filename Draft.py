511ec255c86d925aed37f4a80f67ddf22a4154df
mdelaur3 <79871807+mdelaur3@users.noreply.github.com>
2023-02-01 11:27:40 -0500

    Update Draft.py

--- Draft.py
+++ Draft.py
@@ -1,3 +1,4 @@
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
@@ -6,13 +7,13 @@ class MyApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical')
         btn1 = Button(text='Button 1')
-        btn1 = Button(size_hint=(.4, .5), pos_hint ={'center_x':.5, 'center_y':.5}, text="Scan a label")
+        btn1 = Button(size_hint=(.4, .3), pos_hint ={'center_x':.5, 'center_y':.5}, background_color = (.5, 0, .9, 1), text="Scan a label")
         btn2 = Button(text='Button 2')
-        btn2 = Button(size_hint=(.4, .5), pos_hint ={'center_x':.5, 'center_y':.5}, text="Look up an ingredient")
+        btn2 = Button(size_hint=(.4, .3), pos_hint ={'center_x':.5, 'center_y':.5}, background_color = (.5, 0, .9, 1), text="Look up an ingredient")
         btn3 = Button(text='Button 3')
-        btn3 = Button(size_hint=(.4, .5), pos_hint ={'center_x':.5, 'center_y':.5}, text="How our rating system works")
+        btn3 = Button(size_hint=(.4, .3), pos_hint ={'center_x':.5, 'center_y':.5}, background_color = (.5, 0, .9, 1), text="How our rating system works")
         btn4 = Button(text='Button 4')
-        btn4 = Button(size_hint=(.4, .5), pos_hint ={'center_x':.5, 'center_y':.5}, text="Request an ingredient")
+        btn4 = Button(size_hint=(.4, .3), pos_hint ={'center_x':.5, 'center_y':.5}, background_color = (.5, 0, .9, 1), text="Request an ingredient")
         layout.add_widget(btn1)
         layout.add_widget(btn2)
         layout.add_widget(btn3)
