b5f8eabb8ca2cf4af265aaf5db02fa4073d4bbb5
mdelaur3 <79871807+mdelaur3@users.noreply.github.com>
2023-02-22 21:18:41 -0500

    Update db.py

--- db.py
+++ db.py
@@ -2,23 +2,25 @@ import sqlite3
 import pandas
 conn = sqlite3.connect('test.db')
 
-print("Opened database successfully");
+print("Opened database successfully"); # prints if successful 
 
-df = pandas.read_csv('formu.csv')
+df = pandas.read_csv('formu.csv') # opens csv 
 
 
 drop_table = "DROP TABLE IF EXISTS INGREDIENTS"
-conn.execute(drop_table)
+conn.execute(drop_table) # drops any old table to create table with current csv
 
 create_sql = """CREATE TABLE INGREDIENTS ( INGREDIENT_NAME TEXT, TYPE TEXT, GLUTEN_FREE INT, VEGAN INT, ECO_FRIENDLY INT, LINKS TEXT, SUMMARY TEXT, TESTED INT, EFFECTIVE INT, CATEGORY TEXT)"""
-conn.execute(create_sql)
+conn.execute(create_sql) # creates table with some text and some integers (type 0/1)
 
-df.to_sql('INGREDIENTS', conn, if_exists='replace', index=False)
+df.to_sql('INGREDIENTS', conn, if_exists='replace', index=False) # saves csv to created table in sql
 
-cursor = conn.cursor()
+cursor = conn.cursor() 
 cursor.execute("select * from Ingredients")
 results = cursor.fetchall()
-print (len(results))
+print (len(results)) # prints the length of results (number of rows)
 
+
+#notes for later:
 #like %string%
 #SQlite like statement
