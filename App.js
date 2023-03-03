2880b12e64dfcecfb671d2683a899270389f10c3
emilianodr5 <67346935+emilianodr5@users.noreply.github.com>
2023-03-03 17:35:06 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from 'react-native'; // adds necessary packages
+// import packages
+import * as React from 'react';
+import { StyleSheet, Button, SafeAreaView, View, Text, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+// formatting code
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
 
 const Separator = () => <View style={styles.separator} />; // defines separator between buttons
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
+// home screen
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
     <View>
       <Button // button layout: title, text color, action on press
         title="Scan an Ingredient Label"
@@ -24,7 +45,7 @@ const App = () => (
       <Button
         title="Look up an ingredient"
         color="#ff6355"
-        onPress={() => Alert.alert('Cannot open search')}
+        onPress={() => navigation.navigate('Search Ingredients')}
       />
     </View>
     <Separator />
@@ -52,28 +73,38 @@ const App = () => (
         />
     </View>
   </SafeAreaView>
-);
+  );
+}
 
-// formatting code
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
+function SearchIngredients({ navigation }) {
+  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+  return(
+   <SafeAreaView style={styles.container} >
+    <View>
+    <Button 
+    title="Go back" 
+    onPress={() => navigation.goBack()} />
+    </View>
+   </SafeAreaView>
+  );
+}
 
-export default App;
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="GLOskin" component={HomeScreen} />
+        <Stack.Screen name="Search Ingredients" component={SearchIngredients} />
+      </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
